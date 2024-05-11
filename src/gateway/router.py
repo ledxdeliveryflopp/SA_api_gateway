cae1f8cb4345ed2de1d01b0cb35afb4e303c1f53
LedxDeliveryFlopp <korstim18@gmail.com>
2024-05-11 20:51:55 +0700

    change post method to get method in verify token router

--- src/gateway/router.py
+++ src/gateway/router.py
@@ -5,7 +5,7 @@ from src.gateway.service import TokenService, init_token_service
 gateway_router = APIRouter(prefix="/gateway", tags=["gateway"])
 
 
-@gateway_router.post("/verify/")
+@gateway_router.get("/verify/")
 async def verify_token_router(request: Request, service: TokenService = Depends(
                               init_token_service)):
     """Роутер проверки токена"""
